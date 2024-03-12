9e3317d5d0ab73a92935c28988b1d596ac347ff1
Shresth <rsvp011430@gmail.com>
2024-03-12 22:55:08 +0530

    updated signup changes

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,9 +4,11 @@ import bcryptjs from 'bcryptjs';
 export const signup = async (req, res,next) => {
     // console.log(req.body);
     const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
+    // const hashedPassword = await bcryptjs.hashSync(password, 10);
+    // const newUser = new User({ username, email, password: hashedPassword });
     try {
+        const hashedPassword = await bcryptjs.hashSync(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
         await newUser.save();
         res.status(201).json("User created successfully!");
     }
--- api/index.js
+++ api/index.js
@@ -22,6 +22,9 @@ app.listen(3000, () => {
     console.log("Server is running on port 3000!");
 });
 
+// app.use(express.urlencoded({
+//     extended: true
+// }))
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
