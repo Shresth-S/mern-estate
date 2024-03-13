9244071d20590b4fe60cc7fd8cbc90eaa400e80f
Shresth <rsvp011430@gmail.com>
2024-03-13 17:31:35 +0530

    create signin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from '../models/Users.js'
 import bcryptjs from 'bcryptjs';
+import errorHandler from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res,next) => {
     // console.log(req.body);
@@ -17,3 +19,22 @@ export const signup = async (req, res,next) => {
     }
 
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User not Found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler, (401, 'Inavlid Login Credentials!'));
+        const token = jwt.sign({ id: validUser._id },process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true, expires: new Date(Date.now() + 24 * 60 * 60 * 7) })
+            .status(200)
+            .json(rest); // httpOnly:true means only our website will have access to this cookie , no third party app will have access to it
+    } catch (error) {
+        next(error);
+    }
+}
+
