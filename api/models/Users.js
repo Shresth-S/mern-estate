c58ba7f1ca73e268861712716e8726d11811987c
Shresth <rsvp011430@gmail.com>
2024-04-23 14:04:52 +0530

    create and add OAuth Component

--- api/models/Users.js
+++ api/models/Users.js
@@ -15,7 +15,11 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true,
-    }
+    },
+    avatar: {
+        type: String,
+        default: "https://www.google.com/imgres?q=profile%20images&imgurl=https%3A%2F%2Fcdn-icons-png.flaticon.com%2F512%2F3135%2F3135715.png&imgrefurl=https%3A%2F%2Fwww.flaticon.com%2Ffree-icon%2Fprofile_3135715&docid=cO4ej6ajxZUMVM&tbnid=dJxfuI6obnHK_M&vet=12ahUKEwiV8tmbzdaFAxVhVmwGHRqKAYEQM3oECBwQAA..i&w=512&h=512&hcb=2&ved=2ahUKEwiV8tmbzdaFAxVhVmwGHRqKAYEQM3oECBwQAA"
+    },
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
