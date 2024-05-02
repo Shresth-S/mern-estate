fb4e27a5553b37bf4d66d7b22e2966e801d0dc7c
Shresth <rsvp011430@gmail.com>
2024-05-02 18:18:09 +0530

    add create listing api route

---
+++ api/models/listing.model.js
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+
+const listingSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    regularPrice: {
+        type: Number,
+        required: true,
+    },
+    discountPrice: {
+        type: Number,
+        required: true,
+    },
+    bathrooms: {
+        type: Number,
+        required: true,
+    },
+    bedrooms: {
+        type: Number,
+        required: true,
+    },
+    furnished: {
+        type: Boolean,
+        required: true,
+    },
+    parking: {
+        type: Boolean,
+        required: true,
+    },
+    type: { //rent or sale
+        type: String,
+        required: true,
+    },
+    offer: {
+        type: Boolean,
+        required: true,
+    },
+    imageUrls: {
+        type: Array,
+        required: true,
+    },
+    userRef: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true })
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
+
+
+// {
+// 	"name":"test",
+// 	"description":"test",
+// 	"address":"test",
+// 	"regularPrice":500,
+// 	"discountPrice":500,
+// 	"bathrooms":5,
+// 	"bedrooms":5,
+// 	"furnished":true,
+// 	"parking":true,
+// 	"type":"rent",
+// 	"offer":true,
+// 	"imageUrls":["hegd","sdgj"],
+// 	"userRef": "oweirufvbcnds"
+// }
